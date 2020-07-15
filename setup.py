b022df0bf8db9df1786b18f9c6bb350d1be26ba4
Patrick T. Komiske III <pkomiske@mit.edu>
2020-07-14 21:55:40 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OmniFold',
+    description='Python package for implementing OmniFold unfolding',
+    author='Patrick T. Komiske III and Eric M. Metodiev',
+    author_email='pkomiske@mit.edu',
+    version='0.0.0',
+    url='https://github.com/ericmetodiev/OmniFold',
+    packages=find_packages()
+)
