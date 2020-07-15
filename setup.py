52795b44ab0611ab914ea455e3c200c803d14b63
Patrick T. Komiske III <pkomiske@mit.edu>
2020-07-14 21:56:59 -0400

    updated github url

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='OmniFold',
-    description='Python package for implementing OmniFold unfolding',
-    author='Patrick T. Komiske III and Eric M. Metodiev',
+    name='Wasserstein',
+    description='Python package wrapping C++ code for computing Wasserstein distances',
+    author='Patrick T. Komiske III',
     author_email='pkomiske@mit.edu',
     version='0.0.0',
-    url='https://github.com/ericmetodiev/OmniFold',
+    url='https://github.com/pkomiske/Wasserstein',
     packages=find_packages()
 )
