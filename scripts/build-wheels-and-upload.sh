cdc3a70e987fc6bd5a0f8d285b8a06a6a5cc578f
Patrick T. Komiske III <pkomiske@mit.edu>
2021-06-15 16:44:17 -0400

    introducing github actions

--- scripts/build-wheels-and-upload.sh
+++ scripts/build-wheels-and-upload.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+echo "Preparing to upload to PyPI ..."
+
+echo "Installing cibuildwheel and twine ..."
+python3 -m pip install twine
+
+echo "Building wheels ..."
+python3 -m cibuildwheel --output-dir wheelhouse
 
 # upload to test server
 if [ ! -z $PYPI_TEST ] || [ ! -z $PYPI ]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPITEST
   if [ "$1" = "sdist" ]; then
+    python3 setup.py sdist --formats=gztar
     python3 -m twine upload -r testpypi --skip-existing --verbose dist/*.tar.gz
   fi
   python3 -m twine upload -r testpypi --skip-existing --verbose wheelhouse/*.whl
@@ -13,6 +22,7 @@ fi
 if [ ! -z $PYPI ]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPI
   if [ "$1" = "sdist" ]; then
+    python3 setup.py sdist --formats=gztar
     python3 -m twine upload --skip-existing --verbose dist/*.tar.gz
   fi
   python3 -m twine upload --skip-existing --verbose wheelhouse/*.whl
