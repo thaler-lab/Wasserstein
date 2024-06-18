5b04f9bf0b2c79bbbc22254b6840b6ce4bbdf997
Matthew Feickert <matthew.feickert@cern.ch>
2024-06-18 16:39:11 -0500

    fix: Use Bash double brackets to guard against errors
    * In the event that environment variables being compared against a value
      with -gt are not defined, using Bash's double bracket syntax will properly
      guard the evaluatation and fail, while single brackets may not.

--- scripts/build-wheels-and-upload.sh
+++ scripts/build-wheels-and-upload.sh
@@ -9,9 +9,9 @@ echo "Building wheels ..."
 python3 -m cibuildwheel --output-dir wheelhouse
 
 # upload to test server
-if [ $PYPITEST -gt 0 ]; then
+if [[ $PYPITEST -gt 0 ]]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPITEST
-  if [ "$1" = "sdist" ]; then
+  if [[ "$1" = "sdist" ]]; then
     python3 setup.py sdist --formats=gztar
     python3 -m twine upload -r testpypi --skip-existing --verbose dist/*.tar.gz
   fi
@@ -19,9 +19,9 @@ if [ $PYPITEST -gt 0 ]; then
 fi
 
 # upload to real pypi server
-if [ $PYPI -gt 0 ]; then
+if [[ $PYPI -gt 0 ]]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPI
-  if [ "$1" = "sdist" ]; then
+  if [[ "$1" = "sdist" ]]; then
     python3 setup.py sdist --formats=gztar
     python3 -m twine upload --skip-existing --verbose dist/*.tar.gz
   fi
