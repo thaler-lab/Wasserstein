4e57c1593830350a2be565ba75fc9e0d0f03d83b
Matthew Feickert <matthew.feickert@cern.ch>
2024-06-19 08:06:27 -0500

    fix: Remove numpy install from build wheel script env (#27)
    * NumPy should not be installed manually but as a build dependency it
      should be defined in the build-system requires in pyproject.toml.
    
    * fix: Use Bash double brackets to guard against errors
    
    * In the event that environment variables being compared against a value
      with -gt are not defined, using Bash's double bracket syntax will properly
      guard the evaluatation and fail, while single brackets may not.

--- scripts/build-wheels-and-upload.sh
+++ scripts/build-wheels-and-upload.sh
@@ -2,16 +2,16 @@
 
 echo "Preparing to upload to PyPI ..."
 
-echo "Installing cibuildwheel, twine, and numpy ..."
-python3 -m pip install cibuildwheel twine numpy
+echo "Installing cibuildwheel and twine..."
+python3 -m pip install cibuildwheel twine
 
 echo "Building wheels ..."
 python3 -m cibuildwheel --output-dir wheelhouse
 
 # upload to test server
-if [ $PYPITEST -gt 0 ]; then
+if [[ $PYPITEST -gt 0 ]]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPITEST
-  if [ "$1" = "sdist" ]; then
+  if [[ "$1" = "sdist" ]]; then
     python3 setup.py sdist --formats=gztar
     python3 -m twine upload -r testpypi --skip-existing --verbose dist/*.tar.gz
   fi
@@ -19,9 +19,9 @@ if [ $PYPITEST -gt 0 ]; then
 fi
 
 # upload to real pypi server
-if [ $PYPI -gt 0 ]; then
+if [[ $PYPI -gt 0 ]]; then
   export TWINE_PASSWORD=$TWINE_PASSWORD_PYPI
-  if [ "$1" = "sdist" ]; then
+  if [[ "$1" = "sdist" ]]; then
     python3 setup.py sdist --formats=gztar
     python3 -m twine upload --skip-existing --verbose dist/*.tar.gz
   fi
